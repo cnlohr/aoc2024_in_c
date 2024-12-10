b30441bf97838a70f1fe9b39a55183d9a32b014c
cnlohr <lohr85@gmail.com>
2024-12-09 23:43:08 -0500

    Rice day7b

--- day7/day7b.c
+++ day7/day7b.c
@@ -12,55 +12,55 @@ int rebase10( int64_t n )
 	return r;
 }
 
-// Goal: Get the combination of things as close to nmaxanswer without going over.
-int experiment( int64_t * listremain, int nremain, int64_t nmaxanswer )
+int experiment( int64_t * listremain, int nremain, int64_t answer )
 {
-	if( nremain > 31 ) terror( "too many numbers" );
-	int o;
-	int test;
+	// Compute with operations from left to right.
+	int64_t cribbed[nremain];
+	int cribop[nremain-1];
+	int b = 0;
+	int i;
 
-	int64_t permutations = 1;
-	int k;
-	for( k = 0; k < nremain-1; k++ )
-		permutations *= 3;
+	cribbed[0] = listremain[0];
 
-	// Time in microseconds.
-	// (If tree)Before early exit: 476869
-	// Early exit on overflow: 421730
-	// Switch statement: 574869
-	// Computed Goto + Early Exit: 423552
-	// Computed Goto, No Early check: 388019
-	for( int64_t permutation = 0; permutation < permutations; permutation++ )
-	{
-		int64_t tperm = permutation;		
-		int64_t running = listremain[0];
+	for( i = 0; i < nremain-1; i++ ) cribop[i] = 0;
 
-		o = 1;
-		int tpo;
-		static void *table[] = { &&c0, &&c1, &&c2 };
-		tpo = tperm % 3; tperm/=3;
-		goto *table[tpo];
-c0:
-		running = running * listremain[o];
-		tpo = tperm % 3; tperm/=3;
-		if( ++o == nremain ) goto done;
-		goto *table[tpo];
-c1:
-		running = running + listremain[o];
-		tpo = tperm % 3; tperm/=3;
-		if( ++o == nremain ) goto done;
-		goto *table[tpo];
-c2:
-		int v = listremain[o];
-		running = running * rebase10(v) + v;
-		tpo = tperm % 3; tperm/=3;
-		if( ++o == nremain ) goto done;
-		goto *table[tpo];
-done:
-		if( o == nremain && running == nmaxanswer ) return 1;
-		//printf( " T %08x  %d\n", ntestmask, running );
-	}
-	return 0;
+	goto compute;
+
+	carry:		// Start at the end and enumerate to a new operator.
+		for( b = nremain - 2; ; b-- )
+		{
+			if( b < 0 )
+				return 0;
+
+			int next = (cribop[b] + 1) % 3;
+			cribop[b] = next;
+
+			// If we have to overflow the front, break out.
+			if( next != 0 )
+				break;
+		}
+
+	compute:	// Only compute the values that have changed.
+		b++;
+		int64_t last = cribbed[b-1];
+
+		for( ; b < nremain; b++ )
+		{
+			int op = cribop[b-1];
+			int val = listremain[b];
+			if( op == 0 )
+				last = last + val;
+			else if( op == 1 )
+				last = last * val;
+			else if( op == 2 )
+				last = last * rebase10(val) + val;
+			cribbed[b] = last;
+		}
+
+		if( last == answer )
+			return 1;
+
+		goto carry;
 }
 
 int main()
@@ -95,5 +95,3 @@ int main()
 	printf( "%ld\n", sum );
 }
 
-// Part 2 - tried 1437294992354 no dice.
-// Answer was 271691107779347
